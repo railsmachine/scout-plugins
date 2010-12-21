30638403a434f3ba999ff74c9685cf01556dba44
Josh Nichols <josh@railsmachine.com>
2010-12-21 19:43:52 +0000

    Initial implementation.

---
+++ god_monitoring/god_monitoring.rb
@@ -0,0 +1,20 @@
+class GodMonitoring < Scout::Plugin
+  def build_report
+    god_command = option(:god_command_name) || "god"
+    pgrep_output = `pgrep #{god_command}`
+
+    god_processes = pgrep_output.split(/\n/)
+    report :god_processes => god_processes.size
+    if god_processes.size == 0
+      alert "No god processes found, expected exactly 1"
+    elsif god_processes.size > 1
+      alert "Multiple god processes found, expected exactly 1", pgrep_output
+    end
+
+    god_status_command = option(:god_status_command) || "sudo god status"
+    god_status_output = `#{god_status_command}`
+    unless $?.success?
+      alert "'#{god_status_command}' not returning sane results", god_status_output
+    end
+  end
+end
