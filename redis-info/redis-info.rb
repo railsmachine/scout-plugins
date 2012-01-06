f22f5af14d5c310b0333efa3bfbc75daebc34155
Josh Nichols <josh@technicalpickles.com>
2012-01-06 18:06:45 -0500

    Generate alerts when redis goes down, and when it comes back.

--- redis-info/redis-info.rb
+++ redis-info/redis-info.rb
@@ -31,30 +31,43 @@ class RedisMonitor < Scout::Plugin
                       :db       => option(:db),
                       :password => option(:password),
                       :host     => option(:client_host)
+
+    down_at = memory(:down_at)
+
     begin
       info = redis.info
+
+      report(:uptime_in_hours   => info['uptime_in_seconds'].to_f / 60 / 60)
+      report(:used_memory_in_mb => info['used_memory'].to_i / MEGABYTE)
+      report(:used_memory_in_kb => info['used_memory'].to_i / KILOBYTE)
+
+      counter(:connections_per_sec, info['total_connections_received'].to_i, :per => :second)
+      counter(:commands_per_sec,    info['total_commands_processed'].to_i,   :per => :second)
+
+      # General Stats
+      %w(changes_since_last_save connected_clients connected_slaves bgsave_in_progress).each do |key|
+        report(key => info[key])
+      end
+
+      if option(:lists)
+        lists = option(:lists).split(',')
+        lists.each do |list|
+          report("#{list} list length" => redis.llen(list))
+        end
+      end
+
+      if down_at # it's do
+        alert("OK - Redis is up", "Was down #{(Time.now.to_i - down_at.to_i) / 60} minutes (since #{down_at})")
+      end
+      down_at = nil
     rescue Errno::ECONNREFUSED => error
-      return error( "Could not connect to Redis.",
-                    "Make certain you've specified correct port, DB and password." )
-    end
-
-    report(:uptime_in_hours   => info['uptime_in_seconds'].to_f / 60 / 60)
-    report(:used_memory_in_mb => info['used_memory'].to_i / MEGABYTE)
-    report(:used_memory_in_kb => info['used_memory'].to_i / KILOBYTE)
-
-    counter(:connections_per_sec, info['total_connections_received'].to_i, :per => :second)
-    counter(:commands_per_sec,    info['total_commands_processed'].to_i,   :per => :second)
-
-    # General Stats
-    %w(changes_since_last_save connected_clients connected_slaves bgsave_in_progress).each do |key|
-      report(key => info[key])
-    end
-
-    if option(:lists)
-      lists = option(:lists).split(',')
-      lists.each do |list|
-        report("#{list} list length" => redis.llen(list))
+      if down_at.nil? # if it wasn't down, it is now
+        down_at = Time.now
+        alert("CRITICAL - Redis is down, or incorrect configuration",
+              "Check that redis-server is running, is not blocked by firewalls, and this plugin has been configured with the correct port, DB and password.")
       end
     end
+
+    remember(:down_at, down_at)
   end
 end
