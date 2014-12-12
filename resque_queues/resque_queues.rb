fd4b28443ac379124df6c5f208a43ca8fbfafb0d
Bryan Traywick <bryantraywick@gmail.com>
2014-12-12 16:54:12 -0500

    Limit the number of queues to 20 and allow specifying which queues to monitor.

--- resque_queues/resque_queues.rb
+++ resque_queues/resque_queues.rb
@@ -13,14 +13,24 @@ class ResqueQueues < Scout::Plugin
     name: Namespace
     notes: "Resque namespace: 'resque:production'"
     default:
+  queues:
+    name: Queues
+    notes: "Resque queues to monitor: '*' or 'foo,bar'"
+    default: '*'
   EOS
 
   def build_report
     Resque.redis = option(:redis)
     Resque.redis.namespace = option(:namespace) unless option(:namespace).nil?
+    queues = (option(:queues) || '*').split(',')
+    queues.reject! { |queue| queue == '*' }
 
-    Resque.queues.each do |queue|
+    queue_count = 0
+    Resque.queues.sort.each do |queue|
+      next unless queues.empty? or queues.include?(queue)
+      break unless queue_count < 20
       report queue => Resque.size(queue)
+      queue_count += 1
     end
   end
 
